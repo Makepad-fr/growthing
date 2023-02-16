f2f8af04b166b94997084f53a2121e9b0d8c9f28
kaanyagci <kaan.yagci@makepad.fr>
2023-02-16 14:06:57 +0100

    :recycle: example code refactored
    username and password will be required only if there no existing browser context

--- scrapers/examples/src/linkedin.ts
+++ scrapers/examples/src/linkedin.ts
@@ -2,24 +2,29 @@ import LinkedInJS from '@makepad/linkedinjs';
 
 const LINKEDIN_USERNAME = process.env['LINKEDIN_USERNAME'];
 const LINKEDIN_PASSWORD = process.env['LINKEDIN_PASSWORD'];
+const AUTHENTICATION_CONTEXT_PATH = "./cookies.json";
 
 async function main() {
 
-    if (LINKEDIN_USERNAME !== undefined && LINKEDIN_PASSWORD !== undefined) {
-        const scraper = await LinkedInJS.init({
-            browser: 'firefox',
-            launchOptions: {
-                headless: false
-            },
-            contextOptions: undefined,
-            authenticatedContextPath: './cookies.json',
-            disableAssets: true
-        });
 
-        await scraper.login(LINKEDIN_USERNAME, LINKEDIN_PASSWORD)
-        return;
+    const scraper = await LinkedInJS.init({
+        browser: 'firefox',
+        launchOptions: {
+            headless: false
+        },
+        contextOptions: undefined,
+        authenticatedContextPath: AUTHENTICATION_CONTEXT_PATH,
+        disableAssets: true
+    });
+    if (!scraper.loadedFromExistingAuthenticationContext) {
+        // If browser is not loaded the existing authentication context need to login manually
+        if (LINKEDIN_USERNAME !== undefined && LINKEDIN_PASSWORD !== undefined) {
+
+            await scraper.login(LINKEDIN_USERNAME, LINKEDIN_PASSWORD)
+            return;
+        }
+        console.error('Environment variables LINKEDIN_USERNAME and LINKEDIN_PASSWORD are required');
     }
-    console.error('Environment variables LINKEDIN_USERNAME and LINKEDIN_PASSWORD are required');
 }
 
 main().then();
